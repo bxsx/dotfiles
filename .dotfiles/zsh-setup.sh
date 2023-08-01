856aa727e6602c7ce9190383689a7683331a1cfd
Bart Skowron <bart@bxsx.dev>
2023-08-01 02:28:22 +0200

    Refactor
    (not tested)

--- .dotfiles/zsh-setup.sh
+++ .dotfiles/zsh-setup.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+
+set -eo pipefail
+
 KEEP_ZSHRC=yes RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
-git clone https://github.com/svenXY/timewarrior ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/timewarrior
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/lukechilds/zsh-nvm "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm"
+git clone https://github.com/svenXY/timewarrior "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/timewarrior"
+git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 echo
 while true; do
-	read -p "Install custom fonts for p10k? (Yes|No) " yn
+	read -rp "Install custom fonts for p10k? (Yes|No) " yn
 	case $yn in
 		[Yy]* ) p10k configure; break;;
 		[Nn]* ) echo "OK. Don't forget to install custom fonts from: https://github.com/romkatv/powerlevel10k#manual-font-installation"; break;;
