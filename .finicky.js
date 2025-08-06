3d831e31af5845b7f0efedb462782538442886d2
Bart Skowron <bart@bxsx.dev>
2025-08-06 03:16:47 +0200

    Set up Finicky and uninstall Browserosaurus

--- .finicky.js
+++ .finicky.js
@@ -1,13 +1,17 @@
 export default {
-  defaultBrowser: "Browserosaurus",
+  defaultBrowser: "com.google.Chrome",
   handlers: [
     {
+      match: finicky.matchDomains(["github.com", "gitlab.com"]),
+      browser: "com.google.Chrome"
+    },
+    {
       match: finicky.matchHostnames("open.spotify.com"),
-      browser: "Spotify",
+      browser: "com.spotify.client"
     },
     {
       match: "https://*.slack.com/*",
-      browser: "Slack",
+      browser: "com.tinyspeck.slackmacgap"
     },
     {
       match: "https://*.zoom.us/j/*",
@@ -15,7 +19,44 @@ export default {
     },
     {
       match: "https://linear.app/*",
-      browser: "Linear",
+      browser: "com.linear"
     },
-  ]
+    {
+      match: ({ opener }) =>
+        [
+          "com.apple.AppStore",
+          "com.apple.iCal",
+          "com.apple.Notes",
+          "com.apple.reminders",
+          "com.culturedcode.ThingsMac",
+          "com.ideasoncanvas.mindnode",
+          "com.readdle.smartemail-Mac",
+          "com.sindresorhus.Caprine",
+          "com.whatsapp.desktop",
+          "org.whispersystems.signal-desktop",
+          "ru.keepcoder.Telegram",
+        ].includes(opener.bundleId),
+      browser: "com.apple.Safari"
+    },
+  ],
+  rewrite: [
+    {
+      match: () => true,
+      url: ({ url }) => {
+        const removeKeysStartingWith = ["utm_", "uta_"];
+        const removeKeys = ["fbclid", "gclid"];
+
+        const search = url.search
+          .split("&")
+          .map((parameter) => parameter.split("="))
+          .filter(([key]) => !removeKeysStartingWith.some((startingWith) => key.startsWith(startingWith)))
+          .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
+
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join("=")).join("&"),
+        };
+      },
+    },
+  ],
 };
